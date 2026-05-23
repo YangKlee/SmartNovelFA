79bdd258bfd1cf777047fcf600e8dac281f9a648
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-05-23 16:39:37 +0700

    tạm ổn

--- src/app/layout/shared/header/header.ts
+++ src/app/layout/shared/header/header.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { AuthServices } from '../../../services/auth/auth-services';
-import { CommonModule } from "@angular/common";
-import { of, BehaviorSubject } from "rxjs"
+import { CommonModule, isPlatformBrowser } from "@angular/common";
+import { BehaviorSubject } from "rxjs"
 import { User } from '../../../models/user/user.model';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatMenuModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header implements OnInit {
   public user$ = new BehaviorSubject<User | null>(null);
-  constructor(private authServices: AuthServices) { }
+  constructor(private authServices: AuthServices, private router: Router, @Inject(PLATFORM_ID) private platformId: Object) { }
   ngOnInit() {
-    this.authServices.loadInfoUserLogined()?.subscribe(
-      e => {
-        this.user$.next(e);
-      }
-    )
+    if (isPlatformBrowser(this.platformId)) {
+      this.authServices.loadInfoUserLogined()?.subscribe(
+        e => {
+          this.user$.next(e);
+        }
+      )
+    }
+
+  }
+  doLogout() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem("token");
+      this.user$.next(null); // Cập nhật lại giao diện ngay lập tức
+
+    }
+    this.router.navigate(['/auth/login']); // đá về login
+  }
+
+  logibBtn() {
+    this.router.navigate(['/auth/login']); // đá về login
   }
 }
