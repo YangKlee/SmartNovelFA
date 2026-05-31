8341779384862cf65fba0d3e264785e33fd2a96d
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-05-31 15:20:27 +0700

    wqwq

---
+++ src/app/component/common/novel-lange-item/novel-lange-item.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Novel } from '../../../models/novel/novel.model';
+@Component({
+  selector: 'app-novel-lange-item',
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  templateUrl: './novel-lange-item.html',
+  styleUrl: './novel-lange-item.css',
+})
+export class NovelLangeItem {
+  @Input() novel!: Novel;
+}
--- src/app/models/novel/novel.model.ts
+++ src/app/models/novel/novel.model.ts
@@ -1,2 +1,22 @@
 export class Novel {
+    novelId?: string;
+  title?: string;
+  slug?: string;
+  description?: string; 
+  ageRating?: string;
+  imageNovelUrl?: string;
+  imageBanerNovelUrl?: string;
+  status?: string;
+  uid?: string;
+  viewCount?: number;   
+  authorName?: string;
+  countChapter?: number;
+  countChapterPublic?:number;
+  countChapterDraf?:number;
+  countChapterRemove?:number;
+  novelRating?:number;
+  novelCountComment?:number;
+  likeCount?: number;
+  createTime?: Date | string; 
+  updateTime?: Date | string;
 }
