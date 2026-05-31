0a3167527a098bedcdd492066463cec4923f9e4d
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-05-31 17:15:50 +0700

    đcm cã lò bất đồng bộ

--- src/app/component/author/novel-manager/novel-manager.ts
+++ src/app/component/author/novel-manager/novel-manager.ts
@@ -5,6 +5,7 @@ import { NovelLangeItem } from "../../common/novel-lange-item/novel-lange-item";
 import { CommonModule} from "@angular/common"
 import { PLATFORM_ID, inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-novel-manager',
   imports: [NovelLangeItem, CommonModule],
@@ -12,22 +13,12 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrl: './novel-manager.css',
 })
 export class NovelManager implements OnInit {
-  public novels!: Novel[]
+  public novels$!: Observable<Novel[]>; 
   constructor(private novelServices: NovelServices){};
   private platformId = inject(PLATFORM_ID);
   ngOnInit()
   {
-    this.novelServices.getUserNovel().subscribe({
-      next: (res) => {
-        this.novels = res;
-      },
-      error: (err) => {
-        if (isPlatformBrowser(this.platformId)) {
-          alert('Lấy truyện không thành công!');
-        }
-        //console.error(err.error.Msg);
-      }
-    })
+      this.novels$ = this.novelServices.getUserNovel();
   }
   ngOnDestroy()
   {
