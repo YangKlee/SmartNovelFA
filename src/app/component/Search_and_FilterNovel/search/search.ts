d1bdf35ec3d81c3292fcca7e3e3e5e3b78098a18
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-06-06 12:55:38 +0700

    tạm fix

--- src/app/component/Search_and_FilterNovel/search/search.ts
+++ src/app/component/Search_and_FilterNovel/search/search.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './search.html',
   styleUrl: './search.css'
 })
